5610c31d3ef3382d638a7ed6a4fd83f795e75b0f
marianamalagon11 <marianamalagon11@gmail.com>
2026-02-01 21:57:12 -0500

    copiar el proyecto de maven al repositorio

---
+++ src/main/java/edu/dosw/bitacora/bitacora_DOSW/BitacoraDoswApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.bitacora.bitacora_DOSW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitacoraDoswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitacoraDoswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/bitacora/bitacora_DOSW/BitacoraDoswApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.bitacora.bitacora_DOSW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitacoraDoswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
